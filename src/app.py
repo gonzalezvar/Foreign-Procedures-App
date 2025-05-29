6e7465586cd5a6ecc4406a9a72e8d2e5811e8154
cesararnetta <cesararnetta@gmail.com>
2025-05-29 19:53:37 +0000

    preparando el endpoint para recuperar contraseña

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -18,6 +20,7 @@ from api.routes.errand_types_routes import errand_type_bp
 from api.routes.errand_routes import errand_bp
 from api.routes.favorites_routes import favorite_bp
 from api.routes.offices_routes import offices_bp
+from extensions import mail
 
 # from models import Person
 
@@ -28,6 +31,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 jwt = JWTManager(app)
+mail.init_app(app)
 CORS(app)
 
 app.register_blueprint(user_bp)
@@ -36,6 +40,21 @@ app.register_blueprint(errand_bp)
 app.register_blueprint(favorite_bp)
 app.register_blueprint(offices_bp)
 
+# Configuración de mail para recuperar contraseña
+
+app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = os.getenv("MAILTRAP_USER")
+app.config['MAIL_PASSWORD'] = os.getenv("MAILTRAP_PASS")
+app.config['MAIL_DEFAULT_SENDER'] = 'celfinalproject@gmail.com'
+
+# app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = False
+# app.config['MAIL_USERNAME'] = os.getenv("MAIL_SENDER")
+# app.config['MAIL_PASSWORD'] = os.getenv("APP_MAIL_KEY")
+# app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_SENDER")
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
